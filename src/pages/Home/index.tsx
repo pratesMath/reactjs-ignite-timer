e6370a840bd4f06c344d531ca6af4073cc7d6d5b
pratesMath <developer.prates.matheus@gmail.com>
2026-09-08 15:00:13 -0300

    chore: exploring controlled x uncontrolled alternatives to handle with forms

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -9,15 +9,23 @@ import {
   StartCountdownButton,
   TaskInput,
 } from './styles'
+import { useState } from 'react'
 
 export function Home() {
+  const [task, setTask] = useState('')
+
+  function handleSubmit(event) {
+    setTask(event.target.task.value)
+  }
+
   return (
     <HomeContainer>
-      <form>
+      <form onSubmit={handleSubmit}>
         <FormContainer>
           <label htmlFor="task">Vou trabalhar em</label>
           <TaskInput
             id="task"
+            name="task"
             list="task-suggestions"
             placeholder="Dê um nome para o seu projeto"
           />
@@ -50,7 +58,7 @@ export function Home() {
           <span>0</span>
         </CountdownContainer>
 
-        <StartCountdownButton disabled type="submit">
+        <StartCountdownButton disabled={!task} type="submit">
           <Play size={24} />
           Começar
         </StartCountdownButton>
